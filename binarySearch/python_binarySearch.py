a1857af4dac3e3bff60614ba085988dc92498238
cngondo <devngondo@gmail.com>
2019-08-31 21:37:40 -0500

    python function added

---
+++ binarySearch/python_binarySearch.py
@@ -0,0 +1,25 @@
+# Funstion assumes that list has already been sorted
+# Always start with sorting the list before implementing algorithm
+
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high)/2
+        guess = list[mid]
+
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid + 1
+        else:
+            low = mid + 1
+    return None
+
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 3))
+print(binary_search(my_list, -1))
